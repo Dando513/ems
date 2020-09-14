fa5ccb3d5ff43feb878f88d93ac63994b5b0d26f
Dando513 <49767042+Dando513@users.noreply.github.com>
2020-09-13 20:12:12 -0400

    fixed print syntax

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const { title, allowedNodeEnvironmentFlags } = require("process");
 var connection = mysql.createConnection({
     host: process.env.DB_HOST,
 
-    // Your port; if not 3306
+
     port: 3306,
 
-    // Your username
+    
     user: process.env.DB_USER,
 
-    // Your password
+
     password: process.env.DB_PASS,
     database: "employeesdb"
 });
@@ -76,21 +76,24 @@ function init() {
 function showEmployees() {
     var query = "SELECT * FROM employees";
     connection.query(query, function (err, res) {
-    printTable(res)    
+        printTable(res)
+        init()
     })
 }
 
 function showDepartments() {
     var query = "SELECT * FROM departments";
     connection.query(query, function (err, res) {
-    printTable(res)  
+        printTable(res)
+        init()
     });
 }
 
 function showRoles() {
     var query = "SELECT * FROM roles";
     connection.query(query, function (err, res) {
-    printTable(res)
+        printTable(res)
+        init()
     })
 }
 
@@ -99,7 +102,7 @@ function addEmployees() {
         {
             name: "fAdd",
             type: "input",
-            message: "Enter employee first name" 
+            message: "Enter employee first name"
         },
         {
             name: "lAdd",
@@ -111,7 +114,7 @@ function addEmployees() {
             type: "input",
             message: "Enter role id"
         }
-    ]).then(function(answer) {
+    ]).then(function (answer) {
         var query = "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)";
         connection.query(query, [answer.fAdd, answer.lAdd, answer.rAdd, null], showEmployees);
     })
@@ -121,7 +124,7 @@ function addDepartments() {
     inquirer.prompt({
         name: "departmentAdd",
         type: "input",
-        message: "Enter name of department to be added"
+        message: "Enter department name"
     }).then(function (answer) {
         var query = "INSERT INTO departments (name) VALUES (?)";
         connection.query(query, answer.departmentAdd, showDepartments);
@@ -145,10 +148,10 @@ function addRoles() {
             type: "input",
             message: "Enter department ID"
         }
-    ]).then(function(answer){
+    ]).then(function (answer) {
         var intS = parseInt(answer.sAdd);
         var intId = parseInt(answer.idAdd);
-        var query = "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)";
+        var query = "INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)";
         connection.query(query, [answer.tAdd, intS, intId], showRoles);
     })
 }
@@ -156,39 +159,43 @@ function addRoles() {
 function updateRoles() {
     var idArr = [];
     var roleArr = [];
-    var roleIDArr = [];
-    var roleQuery = "SELECT * FROM role";
+    var roleQuery = "SELECT * FROM roles";
     connection.query(roleQuery, function (err, res) {
         res.forEach(obj => {
-            roleArr.push(obj.title);
-            roleIDArr.push(obj.id);
+            roleArr.push({
+                name: obj.title,
+                value: obj.id
+            })
         })
-    })
-    var idQuery = "SELECT * FROM employee";
-    connection.query(idQuery, function (err, res) {
-        res.forEach(obj => {
-            console.log("id: ", obj.id, " name: ", obj.first_name, " ", obj.last_name);
-            idArr.push(obj.id);
-        });
 
-        inquirer.prompt([
-            {
-                name: "employeeId",
-                type: "list",
-                choices: idArr,
-                message: "Select ID of employee you want to change role for"
-            },
-            {
-                name: "newRole",
-                type: "list",
-                choices: roleArr,
-                message: "Select new role for employee"
-            }
-        ]).then(function (answer) {
-            var changeID = roleIDArr[roleArr.indexOf(answer.newRole)];
-            var query = "UPDATE employee SET role_id = ? WHERE ?";
-            connection.query(query, [changeID, { id: answer.employeeId }], function (err, res) {
-                if (err) throw err;
+        var idQuery = "SELECT * FROM employees";
+        connection.query(idQuery, function (err, res) {
+            res.forEach(obj => {
+                idArr.push({
+                    name: obj.first_name + " " + obj.last_name,
+                    value: obj.id
+                })
+            });
+
+            inquirer.prompt([
+                {
+                    name: "employeeId",
+                    type: "list",
+                    choices: idArr,
+                    message: "Select ID of employee to change role"
+                },
+                {
+                    name: "newRole",
+                    type: "list",
+                    choices: roleArr,
+                    message: "Select new role for employee"
+                }
+            ]).then(function (answer) {
+                var query = "UPDATE employees SET role_id = ? WHERE ?";
+                connection.query(query, [answer.newRole, { id: answer.employeeId }], function (err, res) {
+                    if (err) throw err;
+                    showEmployees()
+                });
             });
         });
     });
